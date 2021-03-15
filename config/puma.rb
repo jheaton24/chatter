99f9c84a0828cb8a6f2f51df78630a28b1b487b9
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-14 22:26:04 -0500

    Adding files for Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -36,8 +36,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
-#
-# preload_app!
+preload_app!
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
