5769100488e1233a411205aab91ce528b3e27240
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-14 20:42:35 -0500

    Routing root to chats#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "chats#index" 
+
   resources :chats
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
