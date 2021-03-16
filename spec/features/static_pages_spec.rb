de0b8212289b090d92938e5d4776b11cb7b60b9c
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-16 16:49:37 -0500

    Adding page title tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -8,21 +8,39 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_selector('title', :text =>
+                                "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit "/static_pages/help"
-      expect(page).to have_content('Help')
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('title', :text =>
+                                "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
       visit "/static_pages/about"
-      expect(page).to have_content('About')
+      expect(page).to have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('title', :text =>
+                                "Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
