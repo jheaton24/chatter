e0a65e22247bac9bc4b732e402273a4fd317ef23
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-17 15:21:56 -0500

    Finishing Chapter 4 Rails excercises

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -15,9 +15,14 @@ describe "Static pages" do
       expect(page).to have_selector('h6', :text => "#{footer_text}")
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).to have_no_title('| Home')
     end
   end
 
