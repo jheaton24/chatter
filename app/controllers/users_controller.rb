da7a6f93a2ef133fc4fa4ac19491831d79aa9ed5
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-19 15:16:42 -0500

    Adding User pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,55 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_user, only: %i[ show edit update destroy ]
+  
+  def index
+    @users = User.all
   end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "User successfully created!" }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user}
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :username)
+    end
 end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
-
+ 
 RSpec.describe "UserPages" do
   subject { page }
+
+  describe "home page" do
+    before { visit users_path }
+
+    it { should have_selector('h1', text: 'Active Users') }
+    it { should have_title(full_title('Users')) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
@@ -9,4 +16,19 @@ RSpec.describe "UserPages" do
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign Up')) }
   end
+
+ # describe "creating invalid users" do
+ #   before { visit signup_path }
+ #   # we don't have validation yet
+ # end
+ #
+ # describe "creating valid users" do
+ #   before { visit signup_path }
+ # 
+ #   fill_in 'Name', with: 'Testy Tester'
+ #   fill_in 'Email', with: 'test@testytester.com'
+ #   fill_in 'UserId', with: 'TestyT123'
+ # 
+ #   click_on 'Submit'
+ # end
 end
