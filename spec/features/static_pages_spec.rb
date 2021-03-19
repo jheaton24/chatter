77d40cba6c16a7e303485e62c94febc3257e3408
Jason Heaton <j_t_heaton@hotmail.com>
2021-03-18 23:42:32 -0500

    Adding bigger test refactoring

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,87 +3,75 @@
 require 'rails_helper'
 
 describe "Static pages" do
+  subject { page }
 
-  let(:base_title) {"Chatter"}
+  let(:base_title) { "Chatter" }
   let(:footer_text) {
-    "©2021, Imperial Sword Creations All rights reserved."}
+    "©2021, Imperial Sword Creations All rights reserved." }
   
   describe "Home page" do
-    it "should have the content 'Chatter'" do
-      visit root_path 
-      expect(page).to have_selector('h1', text: "Welcome to Chatter!") 
-      expect(page).to have_selector('p', :text => "#{footer_text}")
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should have a custom page title" do
-      visit root_path 
-      expect(page).to have_no_title('| Home')
-    end
-
-    it "should have working links" do
-      visit root_path
-      expect(page).to have_link('Home')
-      expect(page).to have_link('Help')
-      expect(page).to have_link('Sign In')
-      expect(page).to have_link('About')
-      expect(page).to have_link('Contact')
-    end
+    before(:each) { visit root_path }
+    
+    it { should have_selector('h1', text: "Welcome to Chatter!") }
+    it { should have_selector('p', text: "#{footer_text}") }     
+    it { should have_title(full_title('')) }
+    it { should have_no_title('| Home') }
+    it { should have_link('Home') }
+    it { should have_link('Help') }
+    it { should have_link('Sign In') }
+    it { should have_link('About') }
+    it { should have_link('Contact') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_selector('h1', text: 'Help')
-      expect(page).to have_selector('p', :text => "#{footer_text}")
-    end
+    before(:each) { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('p', :text => "#{footer_text}") }
+    it { should have_title(full_title('Help')) }
+    it { should have_link('Home') }
+    it { should have_link('Help') }
+    it { should have_link('Sign In') }
+    it { should have_link('About') }
+    it { should have_link('Contact') }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit about_path
-      expect(page).to have_selector('h1', :text => 'About')
-      expect(page).to have_selector('p', :text => "#{footer_text}")
-    end
+    before(:each) { visit about_path }
 
-    it "should have the title 'About'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About")
-    end
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_selector('p', :text => "#{footer_text}") }
+    it { should have_title(full_title('About')) }
+    it { should have_link('Home') }
+    it { should have_link('Help') }
+    it { should have_link('Sign In') }
+    it { should have_link('About') }
+    it { should have_link('Contact') }
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_selector('h1', text: 'Contact Us')
-      expect(page).to have_selector('p', text: "#{footer_text}")
-    end
+    before(:each) { visit contact_path }
 
-    it "should have the title 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_title("#{base_title} | Contact Us")
-    end
+    it { should have_selector('h1', text: 'Contact Us') }
+    it { should have_selector('p', text: "#{footer_text}") }
+    it { should have_title(full_title("Contact Us")) }
+    it { should have_link('Home') }
+    it { should have_link('Help') }
+    it { should have_link('Sign In') }
+    it { should have_link('About') }
+    it { should have_link('Contact') }
   end
 
   describe "Sign In page" do
-    it "should have the content 'Sign In'" do
-      visit signIn_path
-      expect(page).to have_selector('h1', text: 'Sign In')
-      expect(page).to have_selector('p', text: "#{footer_text}")
-    end
+    before(:each) { visit signIn_path }
 
-    it "should have the title 'Sign In'" do
-      visit signIn_path
-      expect(page).to have_title("#{base_title} | Sign In")
-    end
+    it { should have_selector('h1', text: 'Sign In') }
+    it { should have_selector('p', text: "#{footer_text}") }
+    it { should have_title(full_title("Sign In")) }
+    it { should have_link('Home') }
+    it { should have_link('Help') }
+    it { should have_link('Sign In') }
+    it { should have_link('About') }
+    it { should have_link('Contact') }
   end
 end
